f02024dface6547425db5c0305f7b6359e7b5d54
werdffelynir <werdffelynir@gmail.com>
2016-11-25 18:36:02 +0200

    fix

--- events/template.js
+++ events/template.js
@@ -1,164 +1,158 @@
-var App = NamespaceApplication({
-    url: '/',
-    name: 'NamespaceApplication',
-    debug: true,
-    constructsType: false,
+(function (global) {
 
-    // Custom
-    node: {}
-});
+    /**
+     *
+     * @param config
+     * @returns {Template}
+     * @constructor
+     */
+    var Template = function (config) {
+        if (!(this instanceof Template)) return new Template(config);
+        this.path = '';
+        if (typeof config === 'object')
+            for (var prop in config)
+                if (!this.hasOwnProperty(this[prop])) this[prop] = config[prop];
+        this.ajax = Template.ajax;
+        this.loadesCount = 0;
+        this.loades = {};
+    };
 
-App.require('dependence', [
-
-    App.url + 'js/lib/animate.js',
-    App.url + 'js/lib/clicker.js',
-    App.url + 'js/lib/event.manager.js'
-
-], onLibraryLoaded, onRequireError).requireStart('dependence');
-
-function onRequireError(error){
-    console.error('onRequireError', error);
-}
-
-function onLibraryLoaded(list){
-    console.log('Application start!');
-    App.Controller.initialize();
-}
-
-App.namespace('Controller', Controller);
-
-
-function Controller(){
-    var ctr = {};
-
-    /** @namespace App.Controller.initialize */
-    ctr.initialize = function () {
-        App.domLoaded(function () {
-            App.node['header'] = App.query('#header');
-            App.node['navigation'] = App.query('#navigation');
-            App.node['sidebar'] = App.query('#sidebar');
-            App.node['content'] = App.query('#content');
-            App.node['footer'] = App.query('#footer');
-
-            var template = new Template();
-
-            var loadList = [
-                '/events/templates/header.html',
-                '/events/templates/navigation.html',
-                '/events/templates/sidebar.html',
-                '/events/templates/content.html',
-                '/events/templates/footer.html'
-            ];
-
-            function templatesLoaded(list) {
-                list['header'];
-                list['navigation'];
-                list['sidebar'];
-                list['content'];
-                list['footer'];
-
-                template.inject(App.node['header'], 'header');
-                template.inject(App.node['navigation'], 'navigation', {items: 'navigation items data'});
+    /**
+     *
+     * @param selector
+     * @param data
+     * @param append
+     * @returns {*}
+     */
+    Template.prototype.inject = function (selector, data, append) {
+        if (typeof selector === 'string')
+            selector = document.querySelector(selector);
+        if (typeof selector === 'object' && selector.nodeType === Node.ELEMENT_NODE) {
+            if (typeof data === 'string') {
+                selector.innerHTML = (!append) ? data : selector.innerHTML + data;
             }
-
-            template.load(loadList, templatesLoaded);
-
-        })
+            else if (typeof data === 'object' && data.nodeType === Node.ELEMENT_NODE) {
+                if (!append)
+                    selector.textContent = '';
+                selector.appendChild(data);
+            }
+            return selector;
+        }
+        return null;
     };
 
-    return ctr
-}
+    /**
+     *
+     * @param viewString
+     * @param params
+     * @returns {*}
+     */
+    Template.prototype.assign = function (viewString, params) {
+        if (typeof params === 'object')
+            for (var k in params)
+                viewString = viewString.replace(new RegExp('{{' + k + '}}', 'gi'), params[k]);
 
-
-
-
-var Template = function (config) {
-    if (!(this instanceof Template)) return new Template(config);
-    if (typeof config === 'object')
-        for (var prop in config)
-            if (!this.hasOwnProperty(this[prop])) this[prop] = config[prop];
-
-    this.ajax = Template.ajax;
-    this.loades = {};
-};
-/*
-Template.prototype.view = function (name, data) {
-    if (data === undefined) {
-        return this.views[name];
-    } else {
-        this.views[name] = this.createViewObject(name, data);
-    }
-};
-
-Template.prototype.createViewObject = function (name, data) {
-    if (typeof data === 'object' && data.name === name && data.type === 'view')
-        return data;
-    return {name: name, data: data, type: 'view', parent: null,
-        events: {}
-    }
-};*/
-
-Template.prototype.inject = function (to, data, append) {
-
-};
-
-Template.prototype.load = function (url, callback) {
-    if (Array.isArray(url)) {
-
-    } else if (typeof url === 'string') {
-
-
-
-
-    }
-};
-
-Template.ajax = function (config, callback) {
-    var conf = {
-        method:     config.method || 'GET',
-        data:       config.data || {},
-        headers:    config.headers || {},
-        action:     config.action || config.url || document.location
+        return viewString;
     };
-    var xhr = new XMLHttpRequest();
-    var kd, kh, fd = new FormData();
 
-    if (conf.method.toUpperCase() !== 'POST') {
-        conf.action += conf.action.indexOf('?') === -1 ? '?' : '';
-        for (kd in conf.data)
-            conf.action += '&' + kd + '=' + encodeURIComponent(conf.data[kd])
-    } else
-        for (kd in conf.data)
-            fd.append(kd, encodeURIComponent(conf.data[kd]));
-
-    xhr.open (conf.method, conf.action, true);
-    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-    for (kd in conf.headers) {
-        xhr.setRequestHeader(kd, conf.headers[kd]);
-    }
-    xhr.onloadend = function () {
-        if (typeof callback === 'function')
-            callback.call(xhr, xhr.status, xhr.responseText);
+    /**
+     *
+     * @param name
+     * @returns {*}
+     */
+    Template.prototype.parseTemplateName = function (name) {
+        if (name.lastIndexOf('/') !== -1)
+            name = name.substr(name.lastIndexOf('/')+1);
+        if (name.lastIndexOf('.') !== -1)
+            name = name.substr(0, name.lastIndexOf('.'));
+        return name
     };
-    xhr.send(fd);
-    return xhr;
-};
 
-Template.prototype.assign = function (string, data) {
+    /**
+     *
+     * @param name
+     * @returns {*}
+     */
+    Template.prototype.get = function (name) {
+        if (this.loades[name])
+            return this.loades[name];
+        return ''
+    };
 
-};
+    /**
+     *
+     * @param url
+     * @param callback
+     */
+    Template.prototype.internalLoader = function (url, callback) {
+        this.ajax({method:'GET', url: this.path + url}, function (status, data) {
+            if (status === 200) {
+                this.loadesCount --;
+                this.loades[this.parseTemplateName(url)] = data;
+                if (this.loadesCount === 0 && typeof callback === 'function')
+                    callback.call(this, this.loades);
+            } else
+                console.error('Error of loading template. Status code ' + status);
+        }, this);
+    };
 
+    /**
+     *
+     * @param urls
+     * @param callback
+     */
+    Template.prototype.load = function (urls, callback) {
+        if (Array.isArray(urls)) {
+            this.loadesCount = urls.length;
+            for (var i = 0; i < urls.length; i ++) {
+                this.internalLoader(urls[i], callback);
+            }
+        } else if (typeof urls === 'string') {
+            this.internalLoader(urls, callback);
+        }
+    };
 
-/**
- * Base AJAX request. Example:
- *  app.ajax({
-     *      method: 'POST',
-     *      url: '/server.php',
-     *      data: {id:123, name:'UserName'}
-     *  }, function (status, data) {
-     *      console.log(status, data);
-     *  });
- * @param {*} config        {method: 'POST', data: {}, headers: {}, action: '/index'}
- * @param callback          executing event - onloadend. function (status, responseText)
- * @returns {XMLHttpRequest}
- */
+    /**
+     *
+     * @param config
+     * @param callback
+     * @param thisInstance
+     * @returns {XMLHttpRequest}
+     */
+    Template.ajax = function (config, callback, thisInstance) {
+        var conf = {
+            method:     config.method || 'GET',
+            data:       config.data || {},
+            headers:    config.headers || {},
+            action:     config.action || config.url || document.location
+        };
+        var xhr = new XMLHttpRequest();
+        var kd, kh, fd = new FormData();
+
+        if (conf.method.toUpperCase() !== 'POST') {
+            conf.action += conf.action.indexOf('?') === -1 ? '?' : '';
+            for (kd in conf.data)
+                conf.action += '&' + kd + '=' + encodeURIComponent(conf.data[kd])
+        } else
+            for (kd in conf.data)
+                fd.append(kd, encodeURIComponent(conf.data[kd]));
+
+        xhr.open (conf.method, conf.action, true);
+        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        for (kd in conf.headers) {
+            xhr.setRequestHeader(kd, conf.headers[kd]);
+        }
+        xhr.onloadend = function () {
+            if (typeof thisInstance !== 'object') thisInstance = {};
+            thisInstance.XMLHttpRequest = xhr;
+            if (typeof callback === 'function')
+                callback.call(thisInstance, xhr.status, xhr.responseText);
+        };
+        xhr.send(fd);
+        return xhr;
+    };
+
+    global.Template = Template;
+    global.Template.version = '0.0.1';
+
+})(this);
